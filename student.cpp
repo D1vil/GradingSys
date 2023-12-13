370c99b4dc07d2bd9053fde211a50898aeded2e5
D1vil <93873708+D1vil@users.noreply.github.com>
2023-12-14 06:57:25 +0800

    Add files via upload

--- student.cpp
+++ student.cpp
@@ -1,5 +1,6 @@
 #include"os.h"
 #include"student.h"
+#include"function.h"
 #include<iomanip>
 #include<time.h>
 #include<string.h>
@@ -12,9 +13,7 @@ using namespace std;
 void help() {
 	cout.setf(ios::left); //设置对齐方式为left 
 	cout.width(30); //设置宽度，不足用空格填充 
-	cout << "ls" << "Display the current directory listing" << endl;	//列出当前目录清单
-	cout.width(30);
-	cout << "cd" << "Enter the specific directory" << endl;		//前往指定目录
+	cout << "CheckCourse" << "Display the current directory listing" << endl;	//列出当前目录清单,查看当前选修课程（ls)
 	cout.width(30);
 	cout << "viewAssign" << "View the content of the corresponding assignment " << endl;  //查看作业内容
 	cout.width(30);
@@ -25,7 +24,18 @@ void help() {
 }
 
 
-bool Select(int PIAddr, char CourseName[]) {
-	mkdir(PIAddr, CourseName);
+bool ViewAssign(int TAAddr, char* AssignName) {
+	cd_func(TAAddr, AssignName);
+	//cat();
+}
 
+bool submit(int CurAddr, char* AssignName,char* content, char* s_type, char* buf) {
+	mkdir_func(CurAddr, AssignName);
+	touch_func(CurAddr + / AssignName, AssignName, buf);
+	echo_func(CurAddr + / AssignName, content, s_type, buf);
+}
+
+bool ViewScore(int TSAddr, char* AssignName) {
+	cd_func(TSAddr, AssignName);
+	//cat();
 }
--- teacher.cpp
+++ teacher.cpp
@@ -1,5 +1,6 @@
 #include"os.h"
 #include"teacher.h"
+#include"function.h"
 #include<iomanip>
 #include<time.h>
 #include<string.h>
@@ -11,9 +12,7 @@ using namespace std;
 void help() {
 	cout.setf(ios::left); //设置对齐方式为left 
 	cout.width(30); //设置宽度，不足用空格填充 
-	cout << "ls" << "Display the current directory listing" << endl;	//列出当前目录清单
-	cout.width(30);
-	cout << "cd" << "Enter the specific directory" << endl;		//前往指定目录
+	cout << "Checkteach" << "Display the current directory listing" << endl;	//列出当前目录清单,查看教授课程（ls)
 	cout.width(30);
 	cout << "assign" << "Post an assignment" << endl;  //布置作业
 	cout.width(30);
@@ -27,14 +26,18 @@ void help() {
 
 
 
-bool Assign(inode fileinode, int PIAddr, char AssignName[], char buf[]) {//在课程目录下新建作业目录，在作业目录下新建作业文件并写入作业内容到文件中
-	mkdir(PIAddr, AssignName);
-	mkfile(PIAddr + / AssignName, AssignName, buf);
-	writefile(fileinode, PIAddr + / AssignName, buf);
-
+bool Assign(int CurAddr, char* AssignName, char* buf, char* s_type) {//在课程目录下新建作业目录，在作业目录下新建作业文件并写入作业内容到文件中
+	mkdir_func(CurAddr, AssignName);
+	touch_func(CurAddr, AssignName, buf);
+	echo_func(CurAddr, AssignName, s_type, buf);
 }
 
-bool Score(inode fileinode, int PIAddr, char Score[], char buf[]) { //每个作业目录下有每个学生提交作业的目录，里面存了学生的作业文件，教师新建分数文件并写入分数
-	mkfile(PIAddr, Score, buf);
-	writefile(fileinode, PIAddr, buf);
+bool CheckAssign(int SAAddr, char* str, char* AssignName) {
+	cd_func(SAAddr, str);
+	//cat();
+}
+
+bool Score(int CurAddr, char* FileName, char* content, char* buf, char* s_type) { //每个作业目录下有每个学生提交作业的目录，里面存了学生的作业文件，教师新建分数文件并写入分数
+	touch_func(CurAddr, FileName, buf);
+	echo_func(CurAddr, content, s_type, buf);
 }
